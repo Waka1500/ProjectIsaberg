9744a61c88b803e2cbfa9bc65b1ef56245e0d8bc
Waka <karlwall95@hotmail.com>
2018-04-05 00:49:15 +0200

    created sign up screen

--- app/src/main/java/se/ju/waka1500student/projectisaberg/MainActivity.java
+++ app/src/main/java/se/ju/waka1500student/projectisaberg/MainActivity.java
@@ -1,5 +1,6 @@
 package se.ju.waka1500student.projectisaberg;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -33,7 +34,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void registerButtonClicked(View view) {
-
+        System.out.println("Register Button was clicked.");
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 
     public void forgotPasswordClicked(View view) {
